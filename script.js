3b6b0a074b10b0dbaa357cc0b7b4f7dcf70b9dfa
Pro Indies <aksharma21091967@gmail.com>
2022-01-04 10:05:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const apikey = "2fa9121772482abaf348a42a8f853cf6";
+const formEl = document.querySelector("form");
+const details = document.querySelector(".details");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  details.innerHTML = "<h1>Loading...</h1>";
+  const location = e.target.location.value;
+  weatherApp(location);
+  formEl.reset();
+});
+async function weatherApp(location) {
+  const result = await fetchAPI(location);
+  generateHTML(result);
+}
+
+async function fetchAPI(location) {
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+   const res = await fetch(baseURL);
+  const data = await res.json();
+  console.log(data);
+  return data;}
+  
+function generateHTML(data) {
+  const html = `
+  <div class="city">
+  <h1>Weather In - ${data.name},${data.sys.country}</h1>
+  </div>
+
+
+
+
+  <div class="temp">
+  <h1>Avg Temperature: ${data.main.temp}°F</h1>
+  </div>
+<div class="minmaxtemp">
+  <h2>Feels Like: ${data.main.feels_like}</h2> 
+  <h2>Max. Temp: ${data.main.temp}</h2>
+</div>
+<div>Note Temperature is in Fahrenheit</div>
+<div class="minmaxtemp1">
+  <h2>Sky : ${data.weather[0].description}</h2> 
+  <h2>Humidity : ${data.main.humidity}%</h2>
+</div>
+<div class="pi">
+<a href="https://proindies.com"><img width="50%" src="https://proindies.com/img/proindies-logo.png"</a>
+</div>
+  `;
+  details.innerHTML = html;
+}
